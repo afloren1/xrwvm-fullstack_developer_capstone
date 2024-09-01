f56953f84bae8181a4cf017719a29ae5a27e8596
afloren1 <bluemoon66063@gmail.com>
2024-09-01 19:08:19 -0400

    added carmodel and carmake django models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     # path for dealer reviews view
 
     # path for add a review view
-    path(route='logout/', view=views.logout_request, name='logout')
+    path(route='logout/', view=views.logout_request, name='logout'),
+    path(route='get_cars/', view=views.get_cars, name ='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
