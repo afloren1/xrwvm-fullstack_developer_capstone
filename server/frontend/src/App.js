d087066d652c9d8ea6e1733dfd943dee1feb65a7
afloren1 <bluemoon66063@gmail.com>
2024-09-03 15:49:13 -0400

    added react component to show reviews for dealers

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,7 @@
 import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register"
-import Dealers from './components/Dealers/Dealers'
+import Dealer from "./components/Dealers/Dealer"
+import Dealers from './components/Dealers/Dealers';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
