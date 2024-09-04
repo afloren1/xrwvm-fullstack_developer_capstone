a72fbc19a1bf8343b04e910c009c220f525fb1e3
afloren1 <bluemoon66063@gmail.com>
2024-09-04 18:17:55 -0400

    fixing code after linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -10,8 +9,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # Other fields as needed
-
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -24,16 +21,13 @@ class CarModel(models.Model):
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
+    validators=[
+        MaxValueValidator(2023),
+        MinValueValidator(2015)
         ])
-    # Other fields as needed
-
     def __str__(self):
         return self.name  # Return the name as the string representation
         
