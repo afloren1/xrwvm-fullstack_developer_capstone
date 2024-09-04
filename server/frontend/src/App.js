31417fdc9deff7a0f206aec100a90c2dcb23efe2
afloren1 <bluemoon66063@gmail.com>
2024-09-03 21:50:08 -0400

    changed url for microservice sentiment analyzer

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,8 +1,8 @@
 import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register"
 import PostReview from "./components/Dealers/PostReview"
-import Dealers from './components/Dealers/Dealers';
-import Dealer from "./components/Dealers/Dealer";
+import Dealers from './components/Dealers/Dealers'
+import Dealer from "./components/Dealers/Dealer"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
