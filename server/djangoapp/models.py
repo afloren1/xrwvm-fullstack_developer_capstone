6769fcd143e517dbad091338e4e87ee9635230e8
afloren1 <bluemoon66063@gmail.com>
2024-09-04 19:00:49 -0400

    fixing linting errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -28,7 +28,8 @@ class CarModel(models.Model):
     validators=[
         MaxValueValidator(2023),
         MinValueValidator(2015)
-    ])
+    ]
+)
 
     def __str__(self):
         return self.name  # Return the name as the string representation
