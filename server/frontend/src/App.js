dc017ed9c4016d49e485aea9def956146d1dc35a
afloren1 <bluemoon66063@gmail.com>
2024-09-06 20:57:32 -0400

    added the search function to dealership details

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,7 @@ import Register from "./components/Register/Register"
 import PostReview from "./components/Dealers/PostReview"
 import Dealers from './components/Dealers/Dealers'
 import Dealer from "./components/Dealers/Dealer"
+import SearchCars from "./components/Dealers/SearchCars"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/searchcars/:id" element={<SearchCars />} />
     </Routes>
   );
 }
