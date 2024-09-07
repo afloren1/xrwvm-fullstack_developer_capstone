96e6da754fd61277faca6a38b81a1c9172eee072
afloren1 <bluemoon66063@gmail.com>
2024-09-06 20:09:29 -0400

    added a new microservice for the car inventory

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -32,11 +32,16 @@ urlpatterns = [
         route='reviews/dealer/<int:dealer_id>',
         view=views.get_dealer_reviews,
         name='dealer_details'),
+    path(
+        route='get_inventory/<int:dealer_id>',
+        view=views.get_inventory,
+        name='get_inventory'),
 
 
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
     path(route='logout/', view=views.logout_request, name='logout'),
     path(route='get_cars/', view=views.get_cars, name='getcars'),
+    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
